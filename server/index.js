fac6ef653de159252755d3033a0787e9556438b8
skaroweb <skaroweb@gmail.com>
2023-08-09 10:32:27 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-const axios = require("axios");
-const cheerio = require("cheerio");
 const express = require("express");
 const app = express();
 const cors = require("cors");
